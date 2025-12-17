a21f8ef48d255221006432a269f5d262d0080c76
Aurora-Borealiss <125693084+Aurora-Borealiss@users.noreply.github.com>
2025-12-17 11:38:49 +0100

    ADD movie.sql triggers

---
+++ movie_triggers.sql
@@ -0,0 +1,160 @@
+DROP TRIGGER IF EXISTS trg_movie_runtime_insert;
+
+DELIMITER $
+CREATE TRIGGER trg_movie_runtime_insert
+BEFORE INSERT ON movie
+FOR EACH ROW
+BEGIN
+    IF NEW.runtime IS NOT NULL AND
+    NEW.runtime <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Runtime doit étre positif';
+    END IF;  
+END 
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_movie_runtime_update;
+
+DELIMITER $
+CREATE TRIGGER trg_movie_runtime_update
+BEFORE UPDATE ON movie
+FOR EACH ROW
+BEGIN
+    IF NEW.runtime IS NOT NULL AND
+    NEW.runtime <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Runtime doit étre positif';
+    END IF;
+END;
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_movie_rating_insert;
+
+DELIMITER $
+CREATE TRIGGER trg_movie_rating_insert
+BEFORE INSERT ON movie
+FOR EACH ROW
+BEGIN
+    IF NEW.global_rating IS NOT NULL AND
+    NEW.global_rating < 0 OR NEW.global_rating > 10 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Global rating doit étre entre 0 et 10';
+    END IF;
+END;
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_movie_rating_update;
+
+DELIMITER $
+CREATE TRIGGER trg_movie_rating_update
+BEFORE UPDATE ON movie
+FOR EACH ROW
+BEGIN
+    IF NEW.global_rating IS NOT NULL AND
+    NEW.global_rating < 0 OR NEW.global_rating > 10 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Global rating doit étre entre 0 et 10';
+    END IF;
+END;
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_cinema_begin;
+
+DELIMITER $
+CREATE TRIGGER trg_cinema_begin
+BEFORE INSERT ON movie
+FOR EACH ROW
+BEGIN
+    IF NEW.release_year IS NOT NULL AND
+    NEW.release_year < 1888 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'L''année de sortie doit étre postérieure à 1888 (année du premier film)';
+    END IF;
+END;
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_actor_deathday_insert;
+
+DELIMITER $
+CREATE TRIGGER trg_actor_deathday_insert
+BEFORE INSERT ON actor
+FOR EACH ROW
+BEGIN
+    IF NEW.deathday IS NOT NULL AND
+    NEW.deathday < NEW.birthday THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La date de décès ne peut pas être antérieure à la date de naissance';
+    END IF;
+END;
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_actor_deathday_update;
+
+DELIMITER $
+CREATE TRIGGER trg_actor_deathday_update
+BEFORE UPDATE ON actor
+FOR EACH ROW
+BEGIN
+    IF NEW.deathday IS NOT NULL AND
+    NEW.deathday < NEW.birthday THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La date de décès ne peut pas être antérieure à la date de naissance';
+    END IF;
+END
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_diffusion_dates;
+
+DELIMITER $
+CREATE TRIGGER trg_diffusion_dates
+BEFORE INSERT ON movie_diffusion
+FOR EACH ROW
+BEGIN
+    IF NEW.end_diffusion_date IS NOT NULL AND
+    NEW.start_diffusion_date > NEW.end_diffusion_date THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La date de fin de diffusion ne peut pas être antérieure à la date de début de diffusion';
+    END IF;
+END
+$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS trg_actor_movie_life;
+
+DELIMITER $
+CREATE TRIGGER trg_actor_movie_life
+BEFORE INSERT ON movie_actor
+FOR EACH ROW
+BEGIN
+    DECLARE actor_birthday DATETIME;
+    DECLARE actor_deathday DATETIME;
+    DECLARE movie_release_year INT;
+
+    SELECT birthday, deathday INTO actor_birthday, actor_deathday
+    FROM actor
+    WHERE id = NEW.actor_id;
+
+    SELECT release_year INTO movie_release_year
+    FROM movie
+    WHERE id = NEW.movie_id;
+
+    IF actor_birthday IS NOT NULL AND
+       NEW.role IS NOT NULL AND
+       NEW.role = 'Lead' AND
+       actor_deathday IS NOT NULL AND
+       movie_release_year IS NOT NULL AND
+       movie_release_year BETWEEN 1000 AND 9999 AND -- Valid year range
+       movie_release_year > YEAR(actor_deathday) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Un acteur décédé avant l\'année de sortie du film ne peut pas être assigné à un rôle principal';
+    END IF;
+END
+$
+DELIMITER ;
